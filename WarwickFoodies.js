e462bfc954d53d82a3795c5d25607921652b74a7
winnis9718chan <56631014+winnis9718chan@users.noreply.github.com>
2019-12-04 16:55:41 +0000

    testing

--- WarwickFoodies.js
+++ WarwickFoodies.js
@@ -7,3 +7,20 @@ $('.fullBackground').fullClip({
   transitionTime: 1000,
   wait: 3000
 });
+
+/* only execute this script when the document is ready */
+$('index.html').ready(function(){
+  /* function called when you click of the button */
+  $("button").click(function(){
+      
+      /* this if else to change the text in the button */
+      if($("button").text() == "☰"){
+       $("button").text("🞬");
+      }else{
+        $("button").text("☰");
+      }
+    
+    /* this function toggle the visibility of our "li" elements */
+    $("li").toggle("slow");
+  });
+});
