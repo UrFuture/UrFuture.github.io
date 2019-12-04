c275db9df8dd2d9e69eabc5c0211f80c79d533cb
winnis9718chan <56631014+winnis9718chan@users.noreply.github.com>
2019-12-04 14:32:11 +0000

    Update JS

--- WarwickFoodies.js
+++ WarwickFoodies.js
@@ -1,10 +1,9 @@
-<script>
-$(document).ready(function(){
-$("select").material_select();
-var old_html = $("div").html();
-$("div").html("");
-$("div").html(old_html);	// at this point, the old material_select has a buggy effect, where the select dropdown is rendered, but clicking it does not show the UL element.
-$("select").material_select("destroy");	// Naturally, the solution that would cross one's mind at this point would be to destroy and re-initialize the select. However, "destroy" does not destroy the select element that we described above, material_select will work, and therefore we end up with duplicates.
-$("select").material_select();
+$('.fullBackground').fullClip({
+  images: [background_web.jpg]
 });
-</script>
+
+
+$('.fullBackground').fullClip({
+  transitionTime: 1000,
+  wait: 3000
+});
